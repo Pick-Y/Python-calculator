5f078f5d28001c28d4bba2634f57adf86ec49e76
Pick-Y <a.scarcella.as@gmail.com>
2023-08-15 11:27:47 +1000

    Thi is the main file in the program. The program asks for an operation ti perform, which includes the numbers and operation sign

---
+++ main.py
@@ -0,0 +1,7 @@
+from calculations import *
+
+#Using the input method, the program asks the user to 
+number = [input("Insert the operation you wish to calculate: ")]
+
+result = identifyOperation(number)
+print(result)
